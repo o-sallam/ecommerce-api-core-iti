5cf12aab04707a11ddab4b589935bb3a9691b541
o-sallam <omar.sallam.dev@gmail.com>
2025-06-16 15:22:05 +0200

    add some users updates and app.js

--- app.js
+++ app.js
@@ -2,16 +2,15 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth.routes");
-const productRoutes=require('./routes/products.routes');
-const cartRoutes=require('./routes/cart.routes');
+const userRoutes = require("./routes/user.routes");
+const productRoutes = require("./routes/products.routes");
+const cartRoutes = require("./routes/cart.routes");
 //const paymentRoutes=require('./routes/payment.routes');
-
 const categoryRoutes = require("./routes/categories.routes");
-const cors = require('cors');
+const cors = require("cors");
 
 mongoose
   .connect(
-
     "mongodb+srv://admin:12345678iti@cluster0.ix3l1dd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
@@ -22,26 +21,18 @@ mongoose
   });
 
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send('Hello');
-});
-
-
-//Routes
-
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
 app.use("/auth", authRoutes);
-app.use('/products', productRoutes); 
-app.use('/cart', cartRoutes); 
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
 // app.use('/payment', paymentRoutes);
-
-
-
-
- app.use("/categories", categoryRoutes);
+app.use("/categories", categoryRoutes);
 
 app.listen(3000, () => {
   console.log("app is running in port 3000");
 });
-
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user.model");
 
 const getUsers = async (req, res) => {
   const users = await User.find();
